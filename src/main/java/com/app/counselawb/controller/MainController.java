17bf8d8b50e4ba71ac0eb8678020f3b7ca3398bf
infiniment <smnoh0707@gmail.com>
2023-10-13 17:44:06 +0900

    메인페이지 컨트롤러 경로 설정

---
+++ src/main/java/com/app/counselawb/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.app.counselawb.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class MainController {
+    @GetMapping("/")
+    public String goToMain() {
+        return "mainpage/mainpage";
+    }
+}
