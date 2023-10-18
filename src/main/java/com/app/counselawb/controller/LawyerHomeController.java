c8be35e3f5f1639945942a66cf23596cb964a40f
hyuncoding <hyunstwolte@gmail.com>
2023-10-18 23:00:54 +0900

    변호사 정보 페이지 80%이상 완료

--- src/main/java/com/app/counselawb/controller/LawyerHomeController.java
+++ src/main/java/com/app/counselawb/controller/LawyerHomeController.java
@@ -1,12 +1,14 @@
 package com.app.counselawb.controller;
 
 
+import com.app.counselawb.domain.dto.AveragePriceDTO;
 import com.app.counselawb.domain.dto.LawyerFieldDTO;
 import com.app.counselawb.domain.dto.LawyerReplyDTO;
 import com.app.counselawb.domain.dto.LawyerReviewDTO;
 import com.app.counselawb.domain.vo.ExperienceVO;
 import com.app.counselawb.domain.vo.LawyerFieldVO;
 import com.app.counselawb.domain.vo.LawyerVO;
+import com.app.counselawb.domain.vo.MemberVO;
 import com.app.counselawb.service.ConsultingReviewService;
 import com.app.counselawb.service.LawyerHomeService;
 import com.app.counselawb.service.LawyerService;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.lang.reflect.Member;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -35,18 +38,18 @@ public class LawyerHomeController {
     private final LawyerHomeService lawyerHomeService;
 
     @GetMapping("lawyer-home")
-    public void GoToLawyerHome(@RequestParam("lawyerId") Long lawyerId, Model model){
+    public void GoToLawyerHome(@RequestParam("lawyerId") Long lawyerId, Model model, LawyerVO lawyerVO, MemberVO memberVO){
         Optional<LawyerVO> foundLawyer = lawyerService.findByLawyerId(lawyerId);
         StringBuilder sb = new StringBuilder();
         if (foundLawyer.isPresent()){
-            LawyerVO lawyerVO = foundLawyer.get();
-            String lawyerIntroduction = lawyerVO.getLawyerIntroduction();
+            LawyerVO lawyer = foundLawyer.get();
+            String lawyerIntroduction = lawyer.getLawyerIntroduction();
             if (lawyerIntroduction.charAt(lawyerIntroduction.length()-1) == '.'){
                 sb.append(lawyerIntroduction);
                 sb.deleteCharAt(sb.length()-1);
-                lawyerVO.setLawyerIntroduction(sb.toString());
+                lawyer.setLawyerIntroduction(sb.toString());
             }
-            model.addAttribute("selectedLawyer", lawyerVO);
+            model.addAttribute("selectedLawyer", lawyer);
         } else {
             model.addAttribute("selectedLawyer", null);
         }
@@ -68,7 +71,6 @@ public class LawyerHomeController {
         foundCaseReplies = foundCaseReplies.stream().limit(3).collect(Collectors.toList());
         model.addAttribute("caseReplies", foundCaseReplies);
         List<LawyerReviewDTO> foundReviews = lawyerHomeService.findReviewsByLawyerId(lawyerId);
-        List<String> consultingTypes = new ArrayList<>();
         foundReviews.forEach((review) -> {
            String consultingType = review.getConsultingType();
            if (consultingType.equals("PHONE")) consultingType = "전화상담";
@@ -80,12 +82,23 @@ public class LawyerHomeController {
 
     }
 
+    public static double myCalc(int min, int max, double avg, int cur){
+        double result = 0;
+        if (cur == avg) result = 50;
+        else if (cur < avg){
+            result = (cur - min) / (avg - min) * 50;
+        } else {
+            result = 50 + (cur - avg) / (max - avg) * 50;
+        }
+        return result;
+    }
+
     @GetMapping("lawyer-info")
-    public String GoToLawyerInfo(@RequestParam("lawyerId") Long lawyerId, Model model){
+    public String GoToLawyerInfo(@RequestParam("lawyerId") Long lawyerId, Model model, LawyerVO lawyerVO, MemberVO memberVO){
         Optional<LawyerVO> foundLawyer = lawyerService.findByLawyerId(lawyerId);
         if (foundLawyer.isPresent()){
-            LawyerVO lawyerVO = foundLawyer.get();
-            model.addAttribute("selectedLawyer", lawyerVO);
+            LawyerVO lawyer = foundLawyer.get();
+            model.addAttribute("selectedLawyer", lawyer);
         } else {
             model.addAttribute("selectedLawyer", null);
         }
@@ -96,6 +109,42 @@ public class LawyerHomeController {
         model.addAttribute("fields", foundFields);
         List<ExperienceVO> foundExperiences = lawyerService.findCareersByLawyerId(lawyerId);
         model.addAttribute("careers", foundExperiences);
+        AveragePriceDTO averagePriceDTO = lawyerHomeService.findPriceAverages();
+        model.addAttribute("priceInfo", averagePriceDTO);
+        if (foundLawyer.isPresent()){
+            LawyerVO currentLawyer = foundLawyer.get();
+            int callPrice = currentLawyer.getCallPrice();
+            int videoPrice = currentLawyer.getVideoPrice();
+            int visitPrice = currentLawyer.getVisitPrice();
+            if (callPrice != 0 && videoPrice != 0 && visitPrice != 0){
+                double callPriceAvg = averagePriceDTO.getCallPriceAverage();
+                double videoPriceAvg = averagePriceDTO.getVideoPriceAverage();
+                double visitPriceAvg = averagePriceDTO.getVisitPriceAverage();
+                int callMax = averagePriceDTO.getCallPriceMax();
+                int callMin = averagePriceDTO.getCallPriceMin();
+                int videoMax = averagePriceDTO.getVideoPriceMax();
+                int videoMin = averagePriceDTO.getVideoPriceMin();
+                int visitMax = averagePriceDTO.getVisitPriceMax();
+                int visitMin = averagePriceDTO.getVisitPriceMin();
+                double callPercent = myCalc(callMin, callMax, callPriceAvg, callPrice);
+                double videoPercent = myCalc(videoMin, videoMax, videoPriceAvg, videoPrice);
+                double visitPercent = myCalc(visitMin, visitMax, visitPriceAvg, visitPrice);
+                model.addAttribute("callPercent", callPercent);
+                model.addAttribute("videoPercent", videoPercent);
+                model.addAttribute("visitPercent", visitPercent);
+            } else {
+                model.addAttribute("callPercent", null);
+                model.addAttribute("videoPercent", null);
+                model.addAttribute("visitPercent", null);
+            }
+        } else {
+            model.addAttribute("callPercent", null);
+            model.addAttribute("videoPercent", null);
+            model.addAttribute("visitPercent", null);
+        }
+        String lawyerProfilePath = lawyerService.findProfileImage(lawyerId);
+        model.addAttribute("profilePath", lawyerProfilePath);
+
         return "/lawyer-info/lawyer-info";
     }
 
