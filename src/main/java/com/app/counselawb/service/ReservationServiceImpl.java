ed233814ef54e551ed1167a37dec82f6fa1216a1
hyuncoding <hyunstwolte@gmail.com>
2023-10-25 02:09:52 +0900

    상담예약 100% / 결제 기능 100%

---
+++ src/main/java/com/app/counselawb/service/ReservationServiceImpl.java
@@ -0,0 +1,45 @@
+package com.app.counselawb.service;
+
+import com.app.counselawb.domain.vo.CouponVO;
+import com.app.counselawb.domain.vo.PaymentVO;
+import com.app.counselawb.domain.vo.ReservationVO;
+import com.app.counselawb.repository.ReservationDAO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
+public class ReservationServiceImpl implements ReservationService {
+
+    private final ReservationDAO reservationDAO;
+
+    @Override
+    public List<CouponVO> findMyCoupons(Long memberId) {
+        return reservationDAO.readMyCoupons(memberId);
+    }
+
+    @Override
+    public void saveReservation(ReservationVO reservationVO) {
+        reservationDAO.writeReservation(reservationVO);
+    }
+
+    @Override
+    public Long findLatestResvIdByMemberId(Long memberId) {
+        return reservationDAO.readLatestResvIdByMemberId(memberId);
+    }
+
+    @Override
+    public void savePaymentInfo(PaymentVO paymentVO) {
+        reservationDAO.writePaymentInfo(paymentVO);
+    }
+
+    @Override
+    public void reviseCouponToUsed(Long couponId) {
+        reservationDAO.modifyCouponToUsed(couponId);
+    }
+
+}
