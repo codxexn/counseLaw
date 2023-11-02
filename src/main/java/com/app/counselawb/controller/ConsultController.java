13c69806c7eba1ebee8b117505f441103d54faa4
kbj10000595 <kbj1000595@gmail.com>
2023-11-02 21:32:16 +0900

    pr

--- src/main/java/com/app/counselawb/controller/ConsultController.java
+++ src/main/java/com/app/counselawb/controller/ConsultController.java
@@ -34,12 +34,12 @@ public class ConsultController {
         pagination.setTotal(consultingCaseService.selectAllCaseCounts());
         pagination.progress();
         int total = pagination.getEndPage();
-        log.info("pagination ={}",pagination.getEndRow());
-        log.info("pagination ={}",pagination.getStartRow());
+//        log.info("pagination ={}",pagination.getEndRow());
+//        log.info("pagination ={}",pagination.getStartRow());
 
-        System.out.println("페이지 개수= " + total);
+//        System.out.println("페이지 개수= " + total);
         List<ConsultingCaseDTO> consultCase = consultingCaseService.selectAllCase(pagination);
-        log.info("date={}",consultCase);
+//        log.info("date={}",consultCase);
         List<LawyerSidebarDTO> getLawyers = consultingCaseService.getLawyers();
 
 //                log.info(consultCase.toString());
