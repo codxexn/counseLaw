5487c0a987d2ff10815aaf6fcd389cf8ecaa427a
dxexn <codxeun@gmail.com>
2024-05-02 16:45:38 +0900

    update existing work(join, login, logout, etc.)

--- src/main/java/com/app/counselawb/domain/dto/MyFavoriteConsultDTO.java
+++ src/main/java/com/app/counselawb/domain/dto/MyFavoriteConsultDTO.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 public class MyFavoriteConsultDTO {
 //    관심글의 Id
     private Long consultingCaseId;
-//    관심글 제목
+//    관심글 제목git 
     private String consultingCaseTitle;
 //    관심글 원본의 작성 날짜
 //    private LocalDate createDate;
