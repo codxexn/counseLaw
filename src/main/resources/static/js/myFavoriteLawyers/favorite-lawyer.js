3886fbc498163e02f7542e9675baad2e88c007fd
dxexn <codxeun@gmail.com>
2024-06-10 01:34:49 +0900

    변호사 즐겨찾기 추가, 제거 기능 및 내 쿠폰함/즐겨찾기 헤더 추가

--- src/main/resources/static/js/myFavoriteLawyers/favorite-lawyer.js
+++ src/main/resources/static/js/myFavoriteLawyers/favorite-lawyer.js
@@ -1,24 +1,74 @@
-// 변호사 이름 옆 별 누르면 해제
-NodeList.prototype.indexOf = Array.prototype.indexOf;
-const filledStars = document.querySelectorAll(".My-Lawyer-Star-Wrapper .bi-star-fill");
-const emptyStars = document.querySelectorAll(".My-Lawyer-Star-Wrapper .bi-star");
+//  // 변호사 이름 옆 별 누르면 해제
+// NodeList.prototype.indexOf = Array.prototype.indexOf;
+// const filledStars = document.querySelectorAll(".My-Lawyer-Star-Wrapper .bi-star-fill");
+// const emptyStars = document.querySelectorAll(".My-Lawyer-Star-Wrapper .bi-star");
+//
+// filledStars.forEach((star) => {
+//     let starIndex = filledStars.indexOf(star);
+//     star.addEventListener("click", (e) => {
+//         console.log(e.target);
+//         e.preventDefault();
+//         star.style.display = "none";
+//         emptyStars[starIndex].style.display = "inline";
+//     })
+// })
+//
+// emptyStars.forEach((star) => {
+//     let starIndex = emptyStars.indexOf(star);
+//     star.addEventListener("click", (e) => {
+//         console.log(e.target);
+//         e.preventDefault();
+//         star.style.display = "none";
+//         filledStars[starIndex].style.display = "inline";
+//     })
+// })
 
-filledStars.forEach((star) => {
-    let starIndex = filledStars.indexOf(star);
-    star.addEventListener("click", (e) => {
-        console.log(e.target);
-        e.preventDefault();
-        star.style.display = "none";
-        emptyStars[starIndex].style.display = "inline";
-    })
-})
-
-emptyStars.forEach((star) => {
-    let starIndex = emptyStars.indexOf(star);
-    star.addEventListener("click", (e) => {
-        console.log(e.target);
-        e.preventDefault();
-        star.style.display = "none";
-        filledStars[starIndex].style.display = "inline";
-    })
-})
+// 즐겨 찾기 별 이미지 변환
+const starImage = document.querySelector('.star');
+let memberId;
+let url;
+if (starImage != null){
+    starImage.addEventListener("click",
+        (e) =>  {
+            memberId = document.querySelector("input[name=memberId]").value;
+            lawyerId = document.querySelector("input[name=lawyerId]").value;
+            if(e.target.src.includes("yellowStar.png")){
+                url = "/lawyer-home/like-delete?lawyerId=" + lawyerId + "&memberId=" + memberId;
+                fetch(url)
+                    .then(response => response.text())
+                    .then(data => {
+                        if (data === "fail"){
+                            alert("서버 오류가 발생했습니다. 다시 시도해주세요.");
+                            return;
+                        } else if (data === "success"){
+                            alert("즐겨찾기가 해제되었습니다.");
+                        } else {
+                            alert("알 수 없는 오류가 발생했습니다. 다시 시도해주세요.");
+                            return;
+                        }
+                    })
+                    .catch((error) => {
+                        console.log("오류 발생 : " + error);
+                    })
+                e.target.src = "/image/lawyerHome/blankedStarYellow.png";
+            } else {
+                url = "/lawyer-home/like?lawyerId=" + lawyerId + "&memberId=" + memberId;
+                fetch(url)
+                    .then(response => response.text())
+                    .then(data => {
+                        if (data === "fail"){
+                            alert("이미 즐겨찾기한 변호사입니다.");
+                        } else if (data === "success"){
+                            alert("즐겨찾기가 반영되었습니다.");
+                        } else {
+                            alert("알 수 없는 오류가 발생했습니다. 다시 시도해주세요.");
+                            return;
+                        }
+                    })
+                    .catch((error) => {
+                        console.log("오류 발생 : " + error);
+                    })
+                e.target.src = "/image/lawyerHome/yellowStar.png";
+            }
+        });
+}
